925e011dea8304a164c9c141c12d2f02b11a98f6
Posheyla <luisa.virgo.98@hotmail.com>
2022-03-21 00:24:44 -0500

    actualizacion ninja

--- ninja.js
+++ ninja.js
@@ -1,6 +1,6 @@
 
 class Ninja{
-    constructor(nombre,salud,velocidad=3,fuerza=3){
+    constructor(nombre,salud=100,velocidad=3,fuerza=3){
         this.nombre = nombre;
         this.salud = salud;
         this.velocidad = velocidad;
@@ -18,9 +18,32 @@ class Ninja{
     }
 }
 
-let primer = new Ninja('Luisa',20);
+class Sensei extends Ninja{
+    constructor(nombre,salud=200,velocidad=10,fuerza=10,sabiduria=10){
+        super(nombre,salud,velocidad,fuerza);
+        this.sabiduria = sabiduria;
+    }
+
+    sayName(){
+        super.sayName();
+    }
+    showStats(){
+        super.showStats();
+        console.log(this.sabiduria);
+    }
+    speakWisdom(){
+        console.log("Lo que un programador puede hacer en un mes, dos programadores pueden hacerlo en dos meses.");
+        super.drinkSake();
+    }
+}
+
+let primer = new Ninja('Luisa');
+const superSensei = new Sensei("Master Splinter");
 
 primer.sayName();
 primer.showStats();
 primer.drinkSake();
 primer.showStats();
+
+superSensei.speakWisdom();
+superSensei.showStats();
